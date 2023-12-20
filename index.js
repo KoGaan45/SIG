fa264daa27741bf5571c73d3dbfbea62c1e4245b
Loïc <loic.mestais@etu.univ-orleans.fr>
2023-12-20 21:04:40 +0100

    debut filtre

--- index.js
+++ index.js
@@ -76,9 +76,76 @@ app.get('/getPI', async (req, res) => {
     console.log(result);
 });
 
+app.get('/getServices', async (req, res) => {
+    const query = 'SELECT * FROM service';
+    const result = await executeQuery(query);
+    res.json(result);
+    console.log(result);
+});
+
+app.get('/getTypes', async (req, res) => {
+    const query = 'SELECT DISTINCT type FROM public ';
+    const result = await executeQuery(query);
+    res.json(result);
+    console.log(result);
+});
+
+app.get('/getCampus', async (req, res) => {
+    const query = 'SELECT * FROM campus';
+    const result = await executeQuery(query);
+    res.json(result);
+    console.log(result);
+});
+
+app.get('/getMatieres', async (req, res) => {
+    var type = req.query.type;
+    const query = `SELECT DISTINCT nom FROM public WHERE type='${type}'`;
+    const result = await executeQuery(query);
+    res.json(result);
+    console.log(result);
+});
+
+app.get('/getServiceByPI', async (req, res) => {
+    var id = req.query.id;
+    const query = `SELECT * FROM service WHERE monpoint='${id}'`;
+    const result = await executeQuery(query);
+    res.json(result);
+    console.log(result);
+});
+
 app.get('/', (req, res) => {
     const htmlContent = fs.readFileSync('app.html', 'utf8');
     res.send(htmlContent);
+});
+
+app.post('/filtrer', async (req, res) => {
+    const data = req.body;
+    console.log(data);
+
+    var baseQuery = `
+    SELECT DISTINCT pt.idpoint
+    FROM pointinteret AS pt
+    LEFT JOIN service AS s ON s.monpoint = pt.idpoint
+    LEFT JOIN public_service as ps ON s.idservice = ps.service_idservice
+    LEFT JOIN public AS pub ON pub.idpublic = ps.public_idpublic
+    LEFT JOIN campus AS c ON c.idcampus=pt.moncampus
+    `;
+
+    var whereClauses = [];
+    if (data.nomaff) whereClauses.push(`pt.nom = '${data.nomaff}'`);
+    if (data.serviceaff) whereClauses.push(`s.nom = '${data.serviceaff}'`);
+    if (data.campusaff) whereClauses.push(`c.idcampus = '${data.campusaff}'`);
+    if (data.typeaff) whereClauses.push(`pub.type = '${data.typeaff}'`);
+    if (data.matiereaff) whereClauses.push(`pub.nom = '${data.matiereaff}'`);
+
+    var query = baseQuery;
+    if (whereClauses.length) {
+        query += ' WHERE ' + whereClauses.join(' AND ');
+    }
+
+    const result = await executeQuery(query);
+    res.json(result);
+    console.log(result);
 });
 
 app.listen(3000, () => {
